ef37f32a3e8b1c1c7bfaebdff64598808b90b396
foreverestBulat <113981993+foreverestBulat@users.noreply.github.com>
2023-12-17 21:54:33 +0300

    + display alert when a player was shot

---
+++ GameClient/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClient;
+
+public class Message
+{
+    public Color Color { get; set; }
+    public string MessageText { get; set; }
+}
